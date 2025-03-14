7b3a686ed70bdad6d7f44aee587f711d4a757a8d
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2025-03-14 20:15:19 +0530

    Addition of loader in messages page

--- src/components/HelpSupport.vue
+++ src/components/HelpSupport.vue
@@ -16,6 +16,9 @@
       <p class="text-body-1">{{ summarize_text }}</p>
     </div>
     <h1 class="text-h6 font-weight-bold text-primary mb-1">Student Queries:</h1>
+    <div class="custom_loader d-flex flex-column justify-center align-center" v-if="loading">
+      <v-progress-circular :size="62" indeterminate color="primary"></v-progress-circular>
+    </div>
     <v-col cols="12" md="12" v-for="query in queries" :key="query.id">
       <v-sheet class="mb-3 rounded-lg" outlined>
         <v-expansion-panels flat>
@@ -34,12 +37,13 @@
               </div>
             </v-expansion-panel-title>
             <v-expansion-panel-text class="text-primary mt-3">
-              <v-textarea label="Reply here" outlined color="primary" rows="2"
-                class="custom-textarea" v-model="reply"></v-textarea>
+              <v-textarea label="Reply here" outlined color="primary" rows="2" class="custom-textarea"
+                v-model="reply"></v-textarea>
               <div class="d-flex justify-end">
                 <v-btn class="mt-0 mr-2 bg-primary" text="Delete Query" size="large"
                   @click="deletequery(query.id)"></v-btn>
-                <v-btn class="mt-0 bg-primary" text="Send Reply" size="large" type="submit" @click="sendReply(query.id)"></v-btn>
+                <v-btn class="mt-0 bg-primary" text="Send Reply" size="large" type="submit"
+                  @click="sendReply(query.id)"></v-btn>
               </div>
             </v-expansion-panel-text>
           </v-expansion-panel>
@@ -57,7 +61,8 @@ export default {
       summarize_text: "",
       summary_loader: false,
       queries: [],
-      reply:""
+      reply: "",
+      loading: false,
     };
   },
   components: {
@@ -81,6 +86,7 @@ export default {
       }
     },
     async fetchqueries() {
+      this.loading = true
       try {
         const response = await axios.get(
           "https://tnp-portal-backend-tpx5.onrender.com/api/v1/queries"
@@ -89,6 +95,8 @@ export default {
         console.log(this.queries)
       } catch (err) {
         console.log(err);
+      } finally {
+        this.loading = false
       }
     },
     async deletequery(queryId) {
@@ -105,9 +113,9 @@ export default {
       try {
         await axios.post(
           `https://tnp-portal-backend-tpx5.onrender.com/api/v1/queries/${queryId}/reply`,
-          {reply:this.reply}
+          { reply: this.reply }
         );
-        this.reply=''
+        this.reply = ''
       } catch (err) {
         console.log(err);
       }
@@ -120,5 +128,9 @@ export default {
 <style scoped>
 :deep(.v-expansion-panel-text__wrapper) {
   padding: 0 !important;
+}
+
+.custom_loader {
+  height: 50vh;
 }
 </style>
