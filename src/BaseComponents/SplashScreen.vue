e067761de4f1d2c848525a89c21cbbea3755653f
ApurvaK <kulkarniapurva2003@gmail.com>
2025-03-17 23:13:56 +0530

    new change in splash

--- src/BaseComponents/SplashScreen.vue
+++ src/BaseComponents/SplashScreen.vue
@@ -3,7 +3,7 @@
         <v-row class="d-flex justify-center" align="center" style="height: 90vh;">
             <v-col cols="7" class="text-center">
                 <v-row class="d-flex justify-center">
-                    <v-col cols="4">
+                    <v-col cols="6">
                         <v-img src="@/Images/ElevateHire_splash.png" cover></v-img>
                     </v-col>
                 </v-row>
